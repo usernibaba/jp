37dd4bce3850562093a474cba478f520eca0b087
zz <aaaa@example.com>
2020-10-14 18:17:20 +0800

    test

---
+++ app/Model/CategoryModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CategoryModel extends  Model{
+    protected  $table='p_category';
+    protected  $primaryKey='cat_id';
+    public $timestamps=false;
+
+}
+
+
+
+
+?>
